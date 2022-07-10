9029134ee1fc6c37067bfb23d4fe3afcef04dee5
malush0987 <qa0150783@gmail.com>
2022-07-09 21:29:55 -0400

    1

---
+++ src/review08_01/ClassW.java
@@ -0,0 +1,29 @@
+package review08_01;
+
+
+
+class Main  implements MyInterface{
+    @Override
+    public void method1() {
+        System.out.println("implementation of method1");
+    }
+
+    @Override
+    public void method2() {
+        System.out.println("implementation of method12");
+    }
+
+    public static void main(String[]args) {
+        MyInterface sd = new MyInterface();
+
+
+        sd.method1();
+        sd.method2();
+        }
+    }
+
+interface MyInterface {
+    void method1();
+
+    void method2();
+}
